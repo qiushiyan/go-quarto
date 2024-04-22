45288c3bab93bb02753dabe17cc6755953f243ad
Qiushi Yan <qiushi.yann@gmail.com>
2024-04-21 23:38:00 -0500

    Document.Render

--- document.go
+++ document.go
@@ -1,6 +1,13 @@
 package quarto
 
+import "context"
+
 // A document represents a Quarto document
 type Document struct {
 	path string
+}
+
+func (d *Document) Render(ctx context.Context, arg ...string) (*Document, error) {
+	s := append([]string{d.path}, arg...)
+	return Render(ctx, s...)
 }
